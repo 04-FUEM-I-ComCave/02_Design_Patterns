0b8d06e479fef83710750ce76e5c6f89f60a94ef
Qbannes <juanclaudio.bedla@freenet.de>
2025-02-12 08:43:45 +0000

    update

---
+++ com/cc/java/observer/interfaces/Observable.java
@@ -0,0 +1,14 @@
+package com.cc.java.observer.interfaces;
+
+
+public interface Observable {
+
+ // abstrakt, Management der Subscriberliste
+    public abstract void attach(Observer o);
+    public abstract void detatch(Observer o);
+
+    // abstrakt
+    public abstract void notifyObservers();
+
+
+}
